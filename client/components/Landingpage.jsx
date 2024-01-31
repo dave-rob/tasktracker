124357a81e42165a96ebe9f9eef9cd44b5c37bfe
Dave <dlrobjr@gmail.com>
2024-01-31 08:58:41 -0700

    testing images on deployment

--- client/components/Landingpage.jsx
+++ client/components/Landingpage.jsx
@@ -67,7 +67,7 @@ export default function LandingPage(props){
  <section className="bg-white dark:bg-gray-900 ">
     <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
       
-        <img className="w-full block" src='assets/websitedemo.png' alt="dashboard image" />
+        <img className="w-full block" src='/assets/websitedemo.png' alt="dashboard image" />
         <div className="mt-4 md:mt-0">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
             Let us help you stay on top of your tasks.
